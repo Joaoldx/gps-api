fb111bc50d8ae17040c4cf1152e1daca83f2311e
João Domingos <j13domingos@gmail.com>
2024-01-28 22:27:24 -0300

    feat: create checkin controller

---
+++ src/modules/checkin/infra/http/controllers/CheckinController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import ListCheckinService from '@modules/checkin/services/ListCheckinService';
+import CreateCheckinService from '@modules/checkin/services/CreateCheckinService';
+
+export default class CheckinController {
+  public async index(request: Request, response: Response) {
+    const findAll = container.resolve(ListCheckinService);
+
+    const checkins = findAll.execute();
+
+    return response.json(checkins);
+  }
+
+  public async create(request: Request, response: Response) {
+    const { name, cpf, date, type, description } = request.body;
+
+    const createCheckin = container.resolve(CreateCheckinService);
+
+    const checkin = createCheckin.execute({
+      name,
+      cpf,
+      date,
+      type,
+      description,
+    });
+
+    return response.json(checkin);
+  }
+}
