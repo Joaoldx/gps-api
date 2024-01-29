fdc9485a7e5b6efefcd318d0546438fb07029665
João Domingos <j13domingos@gmail.com>
2024-01-29 16:19:44 -0300

    feat: handle with controller validation

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,12 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routers';
+import { dataSource } from '../typeorm';
+import '@shared/infra/typeorm';
+import '@shared/infra/container';
 
 const app = express();
 
@@ -8,7 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
-app.listen(3333, () => {
-  console.log('Servidor inicializou na porta 3333');
+dataSource.initialize().then(() => {
+  app.listen(3333, () => {
+    console.log('Server started on port 3333!');
+  });
 });
