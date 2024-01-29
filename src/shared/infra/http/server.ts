5c3fa8a610b3b251abb2bb7303dffdbd2936df84
João Domingos <j13domingos@gmail.com>
2024-01-29 16:22:05 -0300

    feat: add custom class to handle errors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import { errors } from 'celebrate';
 import routes from './routers';
 import { dataSource } from '../typeorm';
+import AppError from '@shared/errors/AppError';
 import '@shared/infra/typeorm';
 import '@shared/infra/container';
 
@@ -15,6 +16,24 @@ app.use(express.json());
 
 app.use(routes);
 app.use(errors());
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 dataSource.initialize().then(() => {
   app.listen(3333, () => {
